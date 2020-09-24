721102a21aca3ca8ad0ae7dde7a1af46919d671a
angelwong121 <63405380+angelwong121@users.noreply.github.com>
2020-09-24 12:59:28 +0100

    add a new study definition
    define study population

---
+++ analysis/study_definition_af.py
@@ -0,0 +1,30 @@
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
+from common_variables import common_variables
+from codelists import *
+
+
+study = StudyDefinition(
+    default_expectations={
+        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "rate": "exponential_increase",
+        "incidence": 0.7,
+    },
+
+    # STUDY POPULATION
+    population=patients.satisfying(
+        """
+        registration_history AND
+        (age >=18 AND age <= 110) AND
+        imd >0 AND NOT (
+        valvular_AF OR
+        ESRD OR
+        antiphospholipid_syndrome
+        )
+
+        """,
+        registration_history=patients.registered_with_one_practice_between(
+            "2019-02-28", "2020-02-29"
+        ),
+    ),
+    **common_variables
+)
