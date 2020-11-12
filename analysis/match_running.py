6bb64220459b997cbf1e6820317d656039f6e28b
angelwong121 <63405380+angelwong121@users.noreply.github.com>
2020-11-12 13:48:43 +0000

    Update match_running.py

--- analysis/match_running.py
+++ analysis/match_running.py
@@ -3,7 +3,7 @@ from match import match
 # input_af_oac is generated from 02a program
 afmatchgeneralpopulation = {
     "case_csv": "input_af_oac",
-    "match_csv": "input_matching_general_population",
+    "match_csv": "input_general_population",
     "matches_per_case": 1,
     "match_variables": {
         "sex": "category",
