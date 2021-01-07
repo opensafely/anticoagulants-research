f1bb1539bfa915a426f660ffdd7e92e11a333568
angelwong121 <63405380+angelwong121@users.noreply.github.com>
2021-01-07 17:04:36 +0000

    Update study_definition_general_population_wo_exclusion_GP_visit.py
    remove no GP visit exclusion criteria

--- analysis/study_definition_general_population_wo_exclusion_GP_visit.py
+++ analysis/study_definition_general_population_wo_exclusion_GP_visit.py
@@ -24,8 +24,7 @@ study = StudyDefinition(
         registration_history AND
         (age >=18 AND age <= 110) AND
         (sex="M" OR sex="F") AND
-        imd >0 AND 
-        gp_consult_count>0 AND NOT (
+        imd >0 AND NOT (
         af OR
         lmwh_last_four_months OR 
         warfarin_last_four_months OR
