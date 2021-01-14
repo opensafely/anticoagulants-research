81a5541e3bdc875b5ab6c7197b52204934886273
angelwong121 <63405380+angelwong121@users.noreply.github.com>
2021-01-14 14:13:06 +0000

    Update codelists.py
    add has-bled codes

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -129,6 +129,8 @@ valvular_af_codes  = codelist_from_csv(
 
 creatinine_codes = codelist(["XE2q5"], system="ctv3")
 
+has_bled_codes = codelist(["XaY6z"], system="ctv3")
+
 #The following is an imperfect description but left as is for consistency until resolution of https://github.com/ebmdatalab/opencodelists/issues/39
 esrf_codes = codelist_from_csv(
     "codelists/opensafely-chronic-kidney-disease.csv", system="ctv3", column="CTV3ID",
